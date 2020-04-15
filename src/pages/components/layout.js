67c2ad8ce3fdfd28d271ae43a4b02f6c65818da6
Paul Sieber <paul.sieber@gmx.at>
2020-04-15 17:56:00 +0200

    first commit

---
+++ src/pages/components/layout.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import './layout.css'
+
+const Header = styled.header`
+  position: absolute;
+  top: 0;
+  height: 50px;
+  width: 100vw;
+  text-align: center;
+  font-size: 2rem;
+  padding: 10px;
+  background-color: greenyellow;
+`
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  height: 50px;
+  width: 100vw;
+  text-align: center;
+  font-size: 2rem;
+  padding: 10px;
+  background-color: grey;
+`
+
+const Container = styled.div`
+`
+
+const Layout = ({ children }) => {
+  return (
+    <Container>
+      <Header>
+        Wien-Kaffee
+    </Header>
+      <main>
+        {children}
+      </main>
+      <Footer>
+        Yeah
+      </Footer>
+    </Container>
+  )
+}
+
+export default Layout
